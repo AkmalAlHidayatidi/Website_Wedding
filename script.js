4184792a8bb082e8e9927012abce9b0780c54764
AkmalAlHidayatidi <149753209+AkmalAlHidayatidi@users.noreply.github.com>
2023-12-11 17:37:57 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const tubahwarna = document.getElementById('tUbahwarna');
-const heading = document.getElementById('heading');
-const badan = document.getElementById('badan');
-const close = document.getElementById('close')
-tubahwarna.onclick = function(){
-  heading.style.backgroundColor = 'purple';
-  badan.style.backgroundColor = 'purple';
-}
