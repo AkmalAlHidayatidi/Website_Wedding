c9d5473e5d53465ddea7957e2988859026139a71
AkmalAlHidayatidi <149753209+AkmalAlHidayatidi@users.noreply.github.com>
2023-12-11 16:42:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const tubahwarna = document.getElementById('tUbahwarna');
+const heading = document.getElementById('heading');
+const badan = document.getElementById('badan');
+const close = document.getElementById('close')
+tubahwarna.onclick = function(){
+  heading.style.backgroundColor = 'purple';
+  badan.style.backgroundColor = 'purple';
+}
+
+close.onclick = function(){
+  tubahwarna.remove();
+}
